b41b899653ec057ce792992bf23b2771b12624ce
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-02-18 21:43:03 -0600

    Added DungeonLibrary class library and Character class

---
+++ DungeonLibrary/Character.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public abstract class Character
+    {
+        private int _life;
+
+        public string Name { get; set; }
+        public int HitChance { get; set; }
+        public int Block { get; set; }
+        public int MaxLife { get; set; }
+        public int Life
+        {
+            get { return _life; }
+            set
+            {
+                if (value <= MaxLife)
+                {
+                    _life = value;
+                }
+                else
+                {
+                    _life = MaxLife;
+                }
+            }
+        }
+
+        public virtual int CalcBlock()
+        {
+            return Block;
+        }//end CalcBlock
+
+        public virtual int CalcHitChance()
+        {
+            return HitChance;
+        }//end CalcHitChance
+
+        public virtual int CalcDamage()
+        {
+            return 0;
+        }//end CalcDamage
+
+    }//end class
+}//end namespace
