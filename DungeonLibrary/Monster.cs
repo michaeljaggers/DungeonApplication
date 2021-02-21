c32cf81ef883bac6c9f6a521fd03ddf000562986
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-02-21 11:33:53 -0600

    Added Monster class

---
+++ DungeonLibrary/Monster.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Monster : Character
+    {
+        private int _minDamage;
+
+        public int MaxDamage { get; set; }
+        public string Description { get; set; }
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set
+            {
+                if (value > 0 && value <= MaxDamage)
+                {
+                    _minDamage = value;
+                }
+                else
+                {
+                    _minDamage = 1;
+                }
+            }
+        }
+
+        public Monster() { }
+
+        public Monster(string name, int life, int maxLife, int hitChance, int block, int minDamage, int maxDamage, string description)
+        {
+            MaxLife = maxLife;
+            MaxDamage = maxDamage;
+            Name = name;
+            Life = life;
+            HitChance = hitChance;
+            Block = block;
+            MinDamage = minDamage;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("\n===== Monster =====\n" +
+                "{0}\n" +
+                "Life: {1} of {2}\n" +
+                "Damage: {3} to {4}\n" +
+                "Block: {5}\n" +
+                "Description:\n{6}\n",
+                Name, Life, MaxLife, MinDamage, MaxDamage, Block, Description);
+        }//end ToString
+
+        public override int CalcDamage()
+        {
+            Random random = new Random();
+            return random.Next(MinDamage, MaxDamage + 1);
+        }//end CalcDamage
+
+    }//end class
+}//end namespace
