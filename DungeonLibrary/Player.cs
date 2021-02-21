612379804d1d72f6fa79fb8d5dd4b4b6642ed99a
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-02-21 11:59:15 -0600

    Added Player and Weapon classes, Race enum

---
+++ DungeonLibrary/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Player : Character
+    {
+        public Race PlayerRace { get; set; }
+        public Weapon EquippedWeapon { get; set; }
+
+        public Player() { }
+
+        public Player(string name, int hitChance, int block, int life, int maxLife, Race playerRace, Weapon equippedWeapon)
+        {
+            MaxLife = maxLife;
+            Name = name;
+            HitChance = hitChance;
+            Block = block;
+            Life = life;
+            PlayerRace = playerRace;
+            EquippedWeapon = equippedWeapon;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("+++++ {0} +++++\n" +
+                "Life: {1} of {2}\n" +
+                "Hit Chance: {3}%\n" +
+                "Weapon: {4}\n" +
+                "Block: {5}\n" +
+                "Description: {6}\n\n",
+                Name, Life, MaxLife, CalcHitChance(), EquippedWeapon, Block, PlayerRace);
+        }//end ToString
+
+        public override int CalcDamage()
+        {
+            Random random = new Random();
+            int damage = random.Next(EquippedWeapon.MinDamage, EquippedWeapon.MaxDamage + 1);
+            return damage;
+        }//end CalcDamage
+
+        public override int CalcHitChance()
+        {
+            return base.CalcHitChance() + EquippedWeapon.BonusHitChance;
+        }//end CalcHitChance
+
+    }//end class
+}//end namespace
---
+++ DungeonLibrary/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Weapon
+    {
+        private int _minDamage;
+
+        public int MaxDamage { get; set; }
+        public string Name { get; set; }
+        public int BonusHitChance { get; set; }
+        public bool IsTwoHanded { get; set; }
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set
+            {
+                if (value > 0 && value <= MaxDamage)
+                {
+                    _minDamage = value;
+                }
+                else
+                {
+                    _minDamage = 1;
+                }
+            }
+        }
+
+        public Weapon(int minDamage, int maxDamage, string name, int bonusHitChance, bool isTwoHanded)
+        {
+            MaxDamage = maxDamage;
+            MinDamage = minDamage;
+            Name = name;
+            BonusHitChance = bonusHitChance;
+            IsTwoHanded = isTwoHanded;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\t{1} to {2} Damage\n" +
+                "Bonus Hit: {3}%\t{4}",
+                Name, MinDamage, MaxDamage, BonusHitChance, IsTwoHanded ? "Two-Handed" : "One-Handed");
+        }//end ToString
+
+    }//end class
+}//end namespace
